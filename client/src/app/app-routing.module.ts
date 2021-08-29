44b0aa8243952d5912d91d644fe6ce9c24b741f4
Maciej Sosnowicz <sosnowicz.maciej@gmail.com>
2021-08-29 22:50:15 +0100

    angular update + change less to scss + app layout

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import {HomeComponent} from "./home/home.component";
+import { LoginComponent } from './Components/login/login.component';
+import {HomeComponent} from "./Components/home/home.component";
 
 
 const routes: Routes = [
