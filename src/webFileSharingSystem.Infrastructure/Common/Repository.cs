5750266e53288902424fa857f5cfe59b1d17ab35
Maciej Sosnowicz <sosnowicz.maciej@gmail.com>
2021-09-14 14:37:35 +0100

    Add PaginatedList to the repository

--- src/webFileSharingSystem.Infrastructure/Common/Repository.cs
+++ src/webFileSharingSystem.Infrastructure/Common/Repository.cs
@@ -37,7 +37,16 @@ namespace webFileSharingSystem.Infrastructure.Common
         {
             return await ApplySpecification(specification).ToListAsync(cancellationToken);
         }
-        
+
+        public async Task<PaginatedList<TEntity>> PaginatedListAsync(int pageNumber, int pageSize, ISpecification<TEntity>? specification = null, CancellationToken cancellationToken = default)
+        {
+            var querySource = ApplySpecification(specification);
+            var count = await querySource.CountAsync(cancellationToken);
+            var items = await querySource.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync(cancellationToken);
+
+            return new PaginatedList<TEntity>(items, count, pageNumber, pageSize);
+        }
+
         public async Task<bool> ContainsAsync(ISpecification<TEntity>? specification = null, CancellationToken cancellationToken = default)
         {
             return await CountAsync(specification, cancellationToken ) > 0;
