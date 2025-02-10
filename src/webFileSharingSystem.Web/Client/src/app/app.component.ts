334d50f5686f37d0d67743d95b374b259003ab92
Maciej Sosnowicz <sosnowicz.maciej@gmai.com>
2025-02-10 21:31:57 +0100

    Moved old client project to Client_ng12 catalog, and created new Angular v19 client project

--- src/webFileSharingSystem.Web/Client/src/app/app.component.ts
+++ src/webFileSharingSystem.Web/Client/src/app/app.component.ts
@@ -1,45 +1,12 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {NavigationEnd, Router, RouterEvent} from "@angular/router";
-import {filter} from "rxjs/operators";
-import {AuthenticationService} from "./services/authentication.service";
-import {FileExplorerService} from "./services/file-explorer.service";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'webFileSharingSystem';
-  currentRoute: string = "";
-
-  ngOnInit(): void {
-    window.addEventListener("dragover", e => {
-      e && e.preventDefault();
-      e.dataTransfer!.effectAllowed = "none";
-      e.dataTransfer!.dropEffect = "none";
-    }, false);
-    window.addEventListener("drop", e => {
-      e && e.preventDefault();
-      e.dataTransfer!.effectAllowed = "none";
-      e.dataTransfer!.dropEffect = "none";
-    }, false);
-  }
-
-
-  constructor(public router: Router,
-              private authenticationService: AuthenticationService, private fileExplorerService: FileExplorerService) {
-    router.events.pipe(filter(event => event instanceof NavigationEnd))
-      .subscribe(event => {
-        fileExplorerService.updateSearchText('');
-        if (event instanceof RouterEvent) {
-          this.currentRoute = event.url;
-        }
-      });
-  }
-
-  public get authenticated(): boolean {
-    return !!this.authenticationService.currentUserValue
-  }
+  title = 'Client';
 }
-
--- src/webFileSharingSystem.Web/Client_ng12/src/app/app.component.spec.ts
+++ src/webFileSharingSystem.Web/Client_ng12/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
-      providers: [provideExperimentalZonelessChangeDetection()]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Client' title`, () => {
+  it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Client');
+    expect(app.title).toEqual('client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Client');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('client app is running!');
   });
 });
