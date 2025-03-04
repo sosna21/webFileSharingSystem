fb6782469b17d1aaae536d3f9cd009516925ac30
Maciej Sosnowicz <sosnowicz.maciej@gmai.com>
2025-03-04 18:03:45 +0100

    add quota usage progress bar to sidebar

---
+++ src/webFileSharingSystem.Web/Client/src/app/core/pipes/file-size.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'fileSize'
+})
+export class FileSizePipe implements PipeTransform {
+
+  transform(size: number): string {
+    if (size <= 0) {
+      return "0 B"
+    }
+    const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    const unitIndex = Math.floor(Math.log(size) / Math.log(1024));
+    return unitIndex === 0
+      ? (size / Math.pow(1024, unitIndex)).toFixed(0) + ' ' + units[unitIndex]
+      : (size / Math.pow(1024, unitIndex)).toFixed(2) + ' ' + units[unitIndex];
+  };
+}
--- src/webFileSharingSystem.Web/Client/src/app/shared/layout/sidebar-layout/sidebar/sidebar.component.ts
+++ src/webFileSharingSystem.Web/Client/src/app/shared/layout/sidebar-layout/sidebar/sidebar.component.ts
@@ -1,9 +1,11 @@
 import { Component, model } from '@angular/core';
 import { NavLinkComponent } from './nav-link/nav-link.component';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import { SpaceUsageBarComponent } from './space-usage-bar/space-usage-bar.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NavLinkComponent],
+  imports: [NavLinkComponent, NgbProgressbarModule, SpaceUsageBarComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
