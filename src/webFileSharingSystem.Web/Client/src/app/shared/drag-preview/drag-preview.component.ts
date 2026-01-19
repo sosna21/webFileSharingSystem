a26f15862f7e272695f9120564f01f7baa6229d1
Maciej Sosnowicz <sosnowicz.maciej@gmail.com>
2026-01-19 20:06:47 +0100

    Refactor DragPreviewComponent: adjust max-width in SCSS and format displayed names, limit length to 35 chars

--- src/webFileSharingSystem.Web/Client/src/app/shared/drag-preview/drag-preview.component.ts
+++ src/webFileSharingSystem.Web/Client/src/app/shared/drag-preview/drag-preview.component.ts
@@ -1,27 +1,38 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+} from '@angular/core';
 import { AppFile } from '../../core/models/app-file.model';
 import { FileToIconPipe } from '../../core/pipes/file-to-icon.pipe';
-
 
 @Component({
   selector: 'app-drag-preview',
   imports: [FileToIconPipe],
   templateUrl: './drag-preview.component.html',
   styleUrl: './drag-preview.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DragPreviewComponent {
   files = input.required<AppFile[]>();
   // targetDirectory = input.required<AppFile | null>();
 
-  fileNames = computed(() => this.files().map(file => file.fileName));
+  fileNames = computed(() => this.files().map((file) => file.fileName));
   areSameMimeType = computed(() => {
-    const mimeTypes = this.files().map(file => file.mimeType);
-    return mimeTypes.every(mimeType => mimeType === mimeTypes[0]);
+    const mimeTypes = this.files().map((file) => file.mimeType);
+    return mimeTypes.every((mimeType) => mimeType === mimeTypes[0]);
   });
 
   get displayNames() {
-    return this.fileNames().slice(0, 5);
+    return this.fileNames()
+      .slice(0, 5)
+      .map((name) => {
+        if (name.length > 35) {
+          return name.slice(0, 20) + '...' + name.slice(-10);
+        }
+        return name;
+      });
   }
 
   get remainingCount() {
