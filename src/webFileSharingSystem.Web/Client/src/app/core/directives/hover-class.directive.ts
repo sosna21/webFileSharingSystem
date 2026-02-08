e0d4384cc81c302f2ca81aed78cb03e95df681f7
Maciej Sosnowicz <sosnowicz.maciej@gmail.com>
2026-02-08 17:10:53 +0100

    Update Angular dependencies to version 21.1.3 and ng-bootstrap to version 20.0.0 + fix update related issues

--- src/webFileSharingSystem.Web/Client/src/app/core/directives/hover-class.directive.ts
+++ src/webFileSharingSystem.Web/Client/src/app/core/directives/hover-class.directive.ts
@@ -1,17 +1,18 @@
-import { Directive, Input, computed, effect, input, signal } from '@angular/core';
+import { Directive, computed, input, signal } from '@angular/core';
 
 @Directive({
   selector: '[appHoverClass]',
   host: {
     '[class]': 'classMap()',
     '(mouseenter)': 'hover.set(true)',
-    '(mouseleave)': 'hover.set(false)'
-  }
+    '(mouseleave)': 'hover.set(false)',
+  },
 })
 export class HoverClassDirective {
-  private hover = signal(false);
+  readonly hover = signal(false);
   readonly appHoverClass = input.required<string>();
 
-
-  readonly classMap = computed(() => this.hover() ? this.appHoverClass() : '');
+  readonly classMap = computed(() =>
+    this.hover() ? this.appHoverClass() : '',
+  );
 }
