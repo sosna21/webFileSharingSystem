ab1f763d7287327da8bd859bf64adb253b352318
Maciej Sosnowicz <sosnowicz.maciej@gmail.com>
2026-02-18 19:52:52 +0100

    Refactor signal utility functions: enhance debouncedSignal and add retainLastDefined for improved signal handling

--- src/webFileSharingSystem.Web/Client/src/app/core/utils/signal-utils.ts
+++ src/webFileSharingSystem.Web/Client/src/app/core/utils/signal-utils.ts
@@ -1,11 +1,28 @@
-import { Signal } from "@angular/core";
-import { toObservable, toSignal } from "@angular/core/rxjs-interop";
-import { debounceTime } from "rxjs";
+import { linkedSignal, Signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime } from 'rxjs';
 
+export function debouncedSignal<T>(
+  signal: Signal<T>,
+  delay: number,
+  initialValue: T,
+) {
+  return toSignal(toObservable(signal).pipe(debounceTime(delay)), {
+    initialValue,
+  });
+}
 
-export function debouncedSignal<T>(signal: Signal<T>, delay: number, initialValue: T) {
-    return toSignal(
-        toObservable(signal).pipe(debounceTime(delay)),
-        { initialValue }
-    );
+export function retainLastDefined<T>(
+  signal: Signal<T | undefined>,
+  initialValue?: T,
+) {
+  return linkedSignal<T | undefined, T | undefined>({
+    source: () => signal(),
+    computation: (
+      currentValue: T | undefined,
+      previousValue?: { value: T | undefined },
+    ) => {
+      return currentValue ?? previousValue?.value ?? initialValue;
+    },
+  });
 }
