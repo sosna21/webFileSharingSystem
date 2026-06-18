09a8f772c71f945521fb5123fed708c5994a4f33
Maciej Sosnowicz <sosnowicz.maciej@gmail.com>
2026-06-18 21:38:56 +0200

    Enhance file name cell component with improved upload status handling and responsive design

---
+++ src/webFileSharingSystem.Web/Client/src/app/shared/table-cells/file-name-cell/upload-progressbar/upload-progressbar.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  BaseFile,
+  ProgressStatus,
+} from '../../../../core/models/base-file.model';
+
+@Component({
+  selector: 'app-upload-progressbar',
+  imports: [NgbProgressbarModule],
+  templateUrl: './upload-progressbar.component.html',
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UploadProgressbarComponent {
+  readonly ProgressStatus = ProgressStatus;
+  readonly file = input.required<BaseFile>();
+}
